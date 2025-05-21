07d370122b75c993856f118682c490d6fb643e98
Farkhad-K <farhodbek.kamolov2303@gmail.com>
2025-05-21 22:42:16 +0500

    update: deatteching student from parent and some more changes

--- src/EduConnect.Api/Abstractions/RepositoriesAbstractions/IParentsRepository.cs
+++ src/EduConnect.Api/Abstractions/RepositoriesAbstractions/IParentsRepository.cs
@@ -5,5 +5,6 @@ namespace EduConnect.Api.Abstractions.RepositoriesAbstractions;
 public interface IParentsRepository
 {
     ValueTask<bool> AttachToParent(Guid parentId, Guid studentId, CancellationToken cancellationToken = default); 
+    ValueTask<bool> ReomveFromParent(Guid parentId, Guid studentId, CancellationToken cancellationToken = default); 
     ValueTask<Parent> GetChildren(Guid parentId, CancellationToken cancellationToken = default); 
 }
--- src/EduConnect.Api/Controllers/ParentsController.cs
+++ src/EduConnect.Api/Controllers/ParentsController.cs
@@ -11,29 +11,45 @@ namespace EduConnect.Api.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class ParentsController(
-    IParentsService parentsService): ControllerBase
+    IParentsService parentsService) : ControllerBase
 {
-    [HttpGet]
-    [Produces("application/json")]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Parent>))]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async ValueTask<IActionResult> GetAllMyChildren(CancellationToken abortionToken = default)
-    {
-        var parentId = JwtClaimHelper.GetUserIdFromToken(User);
-        if (parentId is null) return Forbid("You do not have permission to access this resource.");
+    // [HttpGet]
+    // [Produces("application/json")]
+    // [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Parent>))]
+    // [ProducesResponseType(StatusCodes.Status404NotFound)]
+    // public async ValueTask<IActionResult> GetAllMyChildren(CancellationToken abortionToken = default)
+    // {
+    //     var parentId = JwtClaimHelper.GetUserIdFromToken(User);
+    //     if (parentId is null) return Forbid("You do not have permission to access this resource.");
 
-        var children = await parentsService.GetChildrenOfParent(parentId.Value, abortionToken);
-        
-        return Ok(children.ToDto());
-    }
+    //     var children = await parentsService.GetChildrenOfParent(parentId.Value, abortionToken);
+
+    //     return Ok(children.ToDto());
+    // }
 
     [Authorize(Roles = "Parent")]
-    [HttpPost]
+    [HttpGet("me")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Parent))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> GetCurrentParent(CancellationToken token = default)
+    {
+        var parentId = JwtClaimHelper.GetUserIdFromToken(User);
+        if (parentId is null)
+            return Unauthorized();
+
+        var parent = await parentsService.GetChildrenOfParent(parentId.Value, token);
+        return Ok(parent.ToDto());
+    }
+
+
+    [Authorize(Roles = "Parent")]
+    [HttpPost("attach-student")]
     [Produces("application/json")]
     [Consumes("application/json")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Parent))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async ValueTask<IActionResult> AttachStudentToParent([FromBody]AttachStudentsToParentDto child, CancellationToken abortionToken = default)
+    public async ValueTask<IActionResult> AttachStudentToParent([FromBody] AttachStudentsToParentDto child, CancellationToken abortionToken = default)
     {
         var parentId = JwtClaimHelper.GetUserIdFromToken(User);
         if (parentId is null) return Forbid("You do not have permission to access this resource.");
@@ -42,6 +58,24 @@ public class ParentsController(
         if (result)
             return Ok("Child attachet to parent successfully.");
 
-        return BadRequest("Failed to attach child to parent.");      
+        return BadRequest("Failed to attach child to parent.");
     }
+
+    [Authorize(Roles = "Parent")]
+    [HttpDelete("detach-student/{studentId:guid}")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async ValueTask<IActionResult> DetachStudentFromParent([FromRoute] Guid studentId, CancellationToken abortionToken = default)
+    {
+        var parentId = JwtClaimHelper.GetUserIdFromToken(User);
+        if (parentId is null) return Forbid("You do not have permission to access this resource.");
+
+        var result = await parentsService.RemoveStudentFromParent(parentId.Value, studentId, abortionToken);
+        if (result)
+            return Ok("Child removed from parent successfully.");
+
+        return BadRequest("Failed to remove child from parent.");
+    }
+
 }
